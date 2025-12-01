10adf262cc64febceac18ed9851a17559b275480
Pînzaru Daniel <pinzaru.danieel@gmail.com>
2025-12-01 21:45:56 +0200

    feat: Initial project structure and setup
    Initializes a multi-package project with a clean architecture consisting of `common`, `data`, `di`, `domain`, and `presentation` layers.
    
    - **`presentation`:** Sets up a new Flutter application, including boilerplate for Android and iOS platforms.
    - **`domain`:** Establishes the core business logic layer with a base `UseCase` and initial models for the `schedule` feature.
    - **`data`:** Implements the data layer, including a `ScheduleRepository` implementation, Retrofit API service for `schedule`, and mappers.
    - **`common`:** Creates a shared package for constants (`ApiConstants`, `Failure`) and utilities.
    - **`di`:** Adds a dependency injection package to handle service and repository setup.
    - **Configuration:** Adds initial `.gitignore`, `analysis_options.yaml`, and `pubspec.yaml` files for each package.

--- presentation/lib/pages/schedule_page/schedule_controller.dart
+++ presentation/lib/pages/schedule_page/schedule_controller.dart
@@ -3,25 +3,24 @@ import 'package:get/get.dart';
 import 'package:get_it/get_it.dart';
 import 'package:presentation/util/mappers/schedule_mapper.dart';
 import 'package:presentation/view_models/schedule_view_model.dart';
-import 'package:uuid/uuid.dart';
 
 class ScheduleController extends GetxController {
- GetScheduleUseCase getScheduleUseCase=GetIt.instance<GetScheduleUseCase>();
- Rxn<ScheduleViewModel> scheduleVm=Rxn();
+  GetScheduleUseCase getScheduleUseCase = GetIt.instance<GetScheduleUseCase>();
+  Rxn<ScheduleViewModel> scheduleVm = Rxn();
 
- void getSchedule() {
-
-   getScheduleUseCase.call(GetScheduleParams('cb41031e-f64e-484b-a38b-3ac7c6995c38')).then((either) {
-     either.fold(
-           (failure) {},
-           (scheduleApi) {
-             scheduleVm.value= scheduleApi.toModel;
-         print('schedule id: ${scheduleVm.value?.groupId}');
-         print('schedule monday: ${scheduleVm.value?.monday[0].subject}');
-         print('schedule room: ${scheduleVm.value?.monday[0].room}');
-       },
-     );
-   });
- }
-
+  void getSchedule() {
+    getScheduleUseCase.call(GetScheduleParams('cb41031e-f64e-484b-a38b-3ac7c6995c38')).then((either) {
+      either.fold(
+        (failure) {
+          print('fail');
+        },
+        (scheduleApi) {
+          scheduleVm.value = scheduleApi.toModel;
+          print('schedule id: ${scheduleVm.value?.groupId}');
+          print('schedule monday: ${scheduleVm.value?.monday[0].subject}');
+          print('schedule room: ${scheduleVm.value?.monday[0].room}');
+        },
+      );
+    });
+  }
 }
