45d5ad99a1dd2d5e6670517c25ec44835ebbaf9b
daniel.pinzaru <daniel.pinzaru@ebs-integrator.com>
2026-08-12 07:59:58 +0300

    feat: update refresh_interceptor to version 0.3.0 and add new dependencies

--- di/lib/modules/data_module.dart
+++ di/lib/modules/data_module.dart
@@ -74,17 +74,15 @@ abstract class DataModule {
     AuthApiService authApiService,
   ) {
     return RefreshInterceptor(
-      tokenStore: TokenStoreAdapter(
-        readAccessToken: authLocalSource.getAccessToken,
-        readRefreshToken: authLocalSource.getRefreshToken,
-        saveTokens: (accessToken, refreshToken) {
-          if (refreshToken == null || refreshToken.isEmpty) {
-            return authLocalSource.insertAccessToken(accessToken);
-          }
-          return authLocalSource.insertTokens(accessToken, refreshToken);
-        },
-        clearTokens: authLocalSource.deleteTokens,
-      ),
+      readAccessToken: authLocalSource.getAccessToken,
+      readRefreshToken: authLocalSource.getRefreshToken,
+      saveTokens: (accessToken, refreshToken) {
+        if (refreshToken == null || refreshToken.isEmpty) {
+          return authLocalSource.insertAccessToken(accessToken);
+        }
+        return authLocalSource.insertTokens(accessToken, refreshToken);
+      },
+      clearTokens: authLocalSource.deleteTokens,
       onRefresh: (refreshToken) async {
         final response = await authApiService.refresh({
           'refresh_token': refreshToken,
